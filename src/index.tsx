0824e3ca9bcae1958f09e1d07b05021f3a90f8c1
Yan Franca <yanguyt@gmail.com>
2022-08-18 10:53:43 -0300

    corrections

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,6 @@
 import './modules/core/styles.css';
-
 export * from './modules/core';
-
 export * from './modules/shared';
-
 export * from './modules/checkout';
 export * from './modules/core';
-
 export * from './modules/auth';
