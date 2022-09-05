9972cd98406add57988bd2f63fd774e80fdcd1e5
Danilo Schleu <daniloschleu@gmail.com>
2022-09-05 11:18:32 -0300

    feat(pe-37):  create connect wallet page (#10)

--- src/modules/shared/enums/PixwayAppRoutes.ts
+++ src/modules/shared/enums/PixwayAppRoutes.ts
@@ -2,6 +2,7 @@ export enum PixwayAppRoutes {
   HOME = '/',
   SIGN_UP = '/auth/signUp',
   SIGN_IN = '/auth/signIn',
+  CONNECT_EXTERNAL_WALLET = '/auth/completeSignup/connectExternalWallet',
   REQUEST_PASSWORD_CHANGE = '/auth/changePassword/request',
   RESET_PASSWORD = '/auth/changePassword/newPassword',
   PROFILE = '/profile',
