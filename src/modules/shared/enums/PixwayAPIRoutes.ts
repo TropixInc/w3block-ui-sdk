9882172738e31704a61cc51bde4eed743423092a
marcoaschmidt <94926117+marcoaschmidt@users.noreply.github.com>
2022-09-15 14:46:54 -0300

    feat(pe-31): Add FAQ Component (#20)
    * added faq component
    
    * code review corrections
    
    * integrate faq on checkout

--- src/modules/shared/enums/PixwayAPIRoutes.ts
+++ src/modules/shared/enums/PixwayAPIRoutes.ts
@@ -5,6 +5,7 @@ export enum PixwayAPIRoutes {
   RESET_PASSWORD = 'auth/reset-password',
   REFRESH_TOKEN = 'auth/refresh-token',
   GET_PROFILE = 'users/profile',
+  GET_FAQ = 'faq/context/filter',
   NFTS_BY_WALLET = 'metadata/nfts/{address}/{chainId}',
   TOKEN_EDITIONS = '{companyId}/token-editions',
   ESTIMATE_TRANSFER_GAS = '{companyId}/token-editions/{id}/estimate-gas/transfer',
