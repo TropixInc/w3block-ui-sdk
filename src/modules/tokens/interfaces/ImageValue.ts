3cf2ecf838ea286e952cc2971731e2b5f6598a19
marcoaschmidt <94926117+marcoaschmidt@users.noreply.github.com>
2022-09-15 14:19:00 -0300

    feat(pe-70): added certificate modal (#25)
    * added certificate modal
    
    * adjusted translation
    
    * correction on import
    
    * integrated certificate with tokenCard
    
    * cr adjust

---
+++ src/modules/tokens/interfaces/ImageValue.ts
@@ -0,0 +1,6 @@
+export interface ImageValue {
+  file?: File;
+  url?: string;
+}
+
+export type MultipleImageValue = Array<ImageValue>;
--- src/modules/tokens/providers/TokenActionsProvider/TokenActionsProvider.tsx
+++ src/modules/tokens/providers/TokenActionsProvider/TokenActionsProvider.tsx
@@ -1,29 +1,52 @@
 import { ReactNode, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { CertificateIssuanceController } from '../../../shared/components/CertificateIssuanceController';
 import { useModalController } from '../../../shared/hooks/useModalController';
 import TokenTransferController from '../../components/TokenTransferController/TokenTransferController';
 import {
   ITokenActionContext,
   TokenActionsContext,
 } from '../../contexts/TokenActionsContext';
+import { usePublicTokenData } from '../../hooks/usePublicTokenData';
 
 interface Props {
   children?: ReactNode;
   collectionId: string;
   collectionName: string;
+  contractAddress: string;
+  name: string;
+  image: string;
+  chainId: number;
+  tokenId: string;
 }
 
 export const TokenActionsProvider = ({
   children,
   collectionId,
   collectionName,
+  contractAddress,
+  name,
+  image,
+  chainId,
+  tokenId,
 }: Props) => {
   const [translate] = useTranslation();
+  const { data: publicTokenResponse } = usePublicTokenData({
+    chainId: chainId.toString(),
+    tokenId,
+    contractAddress,
+  });
   const {
     isOpen: isOpenTransferModal,
     openModal: openTransferModal,
     closeModal: closeTransferModal,
+  } = useModalController();
+
+  const {
+    isOpen: isOpenCertificateModal,
+    openModal: openCertificateModal,
+    closeModal: closeCertificateModal,
   } = useModalController();
 
   const value = useMemo<ITokenActionContext>(
@@ -41,11 +64,11 @@ export const TokenActionsProvider = ({
         label: translate('tokens>tokenCardActions>certificate'),
         disabled: false,
         onClick: () => {
-          //
+          openCertificateModal();
         },
       },
     ],
-    [openTransferModal, translate]
+    [openTransferModal, openCertificateModal, translate]
   );
   return (
     <TokenActionsContext.Provider value={value}>
@@ -61,6 +84,20 @@ export const TokenActionsProvider = ({
             number: '1',
           },
         ]}
+      />
+      <CertificateIssuanceController
+        isOpen={isOpenCertificateModal}
+        onClose={closeCertificateModal}
+        transactionHash={publicTokenResponse?.data.edition.mintedHash ?? ''}
+        contractAddress={contractAddress}
+        name={name}
+        image={image}
+        description={publicTokenResponse?.data.information.description ?? ''}
+        originalOwnerWalletAddress={
+          publicTokenResponse?.data.token?.firstOwnerAddress ?? ''
+        }
+        chainId={chainId}
+        tokenId={tokenId}
       />
     </TokenActionsContext.Provider>
   );
