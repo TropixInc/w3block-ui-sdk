f7a6acc68a527a660cbdf3813616e9d4ae2ba5b3
Percegaroli <56946412+Percegaroli@users.noreply.github.com>
2022-09-23 17:32:34 -0300

    feat(Tk 603): adapt sdk to use in post sign up flow in key (#40)

--- src/modules/auth/components/VerifySignUpTokenExpired/VerifySignUpTokenExpired.tsx
+++ src/modules/auth/components/VerifySignUpTokenExpired/VerifySignUpTokenExpired.tsx
@@ -1,19 +1,31 @@
 import { useEffect } from 'react';
 import { Trans } from 'react-i18next';
 
+import classNames from 'classnames';
+
+import { PixwayAppRoutes } from '../../../shared/enums/PixwayAppRoutes';
 import { useCompanyConfig } from '../../../shared/hooks/useCompanyConfig';
 import useTranslation from '../../../shared/hooks/useTranslation';
 import { ReactComponent as MailError } from '../../assets/icons/mailError.svg';
 import { useRequestConfirmationMail } from '../../hooks/useRequestConfirmationMail';
 import { AuthFooter } from '../AuthFooter';
-import { AuthLayoutBase } from '../AuthLayoutBase';
+import { AuthLayoutBase, AuthLayoutBaseClasses } from '../AuthLayoutBase';
 
 interface Props {
   email: string;
   onSendEmail?: () => void;
+  tenantId?: string;
+  classes?: AuthLayoutBaseClasses;
+  isPostSignUp?: boolean;
 }
 
-export const VerifySignUpTokenExpired = ({ email, onSendEmail }: Props) => {
+export const VerifySignUpTokenExpired = ({
+  email,
+  onSendEmail,
+  tenantId,
+  classes = {},
+  isPostSignUp = false,
+}: Props) => {
   const { logoUrl } = useCompanyConfig();
   const { mutate, isLoading, isSuccess } = useRequestConfirmationMail();
   const [translate] = useTranslation();
@@ -23,14 +35,20 @@ export const VerifySignUpTokenExpired = ({ email, onSendEmail }: Props) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isSuccess]);
 
+  const callbackPath = isPostSignUp
+    ? PixwayAppRoutes.COMPLETE_SIGNUP
+    : PixwayAppRoutes.SIGN_UP_MAIL_CONFIRMATION;
+
   return (
     <AuthLayoutBase
       classes={{
-        root: '!pw-px-5 sm:!pw-px-5',
-        contentContainer:
+        root: classNames('!pw-px-5 sm:!pw-px-5', classes.root ?? ''),
+        contentContainer: classNames(
           '!pw-pt-0 sm:!pw-pt-[35px] sm:!pw-px-[35px] !pw-shadow-none sm:!pw-shadow-[1px_1px_10px_rgba(0,0,0,0.2)] !pw-bg-transparent sm:!pw-bg-white !pw-px-0 sm:!pw-px-[35px] !pw-max-w-none sm:!pw-max-w-[514px]',
-        logo: 'w-[130px] h-[130px]',
-        title: 'pw-px-2 sm:pw-px-0',
+          classes.contentContainer ?? ''
+        ),
+        logo: classNames('w-[130px] h-[130px]', classes.logo ?? ''),
+        title: classNames('pw-px-2 sm:pw-px-0', classes.title ?? ''),
       }}
       logo={logoUrl}
       title={translate('auth>expiredLink>stepTitle')}
@@ -43,7 +61,7 @@ export const VerifySignUpTokenExpired = ({ email, onSendEmail }: Props) => {
         <span className="pw-text-brand-primary pw-text-sm pw-leading-[21px]">
           <Trans i18nKey="auth>emailConfirmation>resendEmailAction">
             <button
-              onClick={() => mutate({ email })}
+              onClick={() => mutate({ email, tenantId, callbackPath })}
               disabled={isLoading}
               className="pw-mb-[29px] pw-font-semibold pw-text-sm pw-leading-[17px] pw-text-brand-primary pw-underline"
             >
