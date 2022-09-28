7538389c0041c25688dd42e6bd5eb6e8f90bc397
marcoaschmidt <94926117+marcoaschmidt@users.noreply.github.com>
2022-09-28 15:30:41 -0300

    feat(pe-98): adjusted certificate to new method (#43)
    * adjusted certificate to new method
    
    * adjusts
    
    * adjusts

--- src/modules/core/providers/W3blockApiProvider/W3blockApiProvider.tsx
+++ src/modules/core/providers/W3blockApiProvider/W3blockApiProvider.tsx
@@ -5,12 +5,14 @@ interface Props {
   w3blockKeyAPIUrl: string;
   w3blockIdAPIUrl: string;
   w3blockCommerceAPIUrl: string;
+  w3blockPdfAPIUrl: string;
 }
 
 export const W3blockAPIContext = createContext({
   w3blockKeyAPIUrl: '',
   w3blockIdAPIUrl: '',
   w3blockCommerceAPIUrl: '',
+  w3blockPdfAPIUrl: '',
 });
 
 export const W3blockApiProvider = ({
@@ -18,14 +20,21 @@ export const W3blockApiProvider = ({
   w3blockIdAPIUrl,
   w3blockKeyAPIUrl,
   w3blockCommerceAPIUrl,
+  w3blockPdfAPIUrl,
 }: Props) => {
   const value = useMemo(() => {
     return {
       w3blockKeyAPIUrl,
       w3blockIdAPIUrl,
       w3blockCommerceAPIUrl,
+      w3blockPdfAPIUrl,
     };
-  }, [w3blockIdAPIUrl, w3blockKeyAPIUrl, w3blockCommerceAPIUrl]);
+  }, [
+    w3blockIdAPIUrl,
+    w3blockKeyAPIUrl,
+    w3blockCommerceAPIUrl,
+    w3blockPdfAPIUrl,
+  ]);
   return (
     <W3blockAPIContext.Provider value={value}>
       {children}
