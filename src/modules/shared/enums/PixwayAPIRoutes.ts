b129fca435cab6fd115422b1fb5581c746f6b25c
yanguyt <yanguyt@hotmail.com>
2022-10-17 12:05:20 -0300

    FAQ corrections

--- src/modules/shared/enums/PixwayAPIRoutes.ts
+++ src/modules/shared/enums/PixwayAPIRoutes.ts
@@ -5,7 +5,7 @@ export enum PixwayAPIRoutes {
   RESET_PASSWORD = 'auth/reset-password',
   REFRESH_TOKEN = 'auth/refresh-token',
   GET_PROFILE = 'users/profile',
-  GET_FAQ = 'faq/context/filter',
+  GET_FAQ = 'companies/{companyId}/faq',
   NFTS_BY_WALLET = 'metadata/nfts/{address}/{chainId}',
   TOKEN_EDITIONS = '{companyId}/token-editions',
   ESTIMATE_TRANSFER_GAS = '{companyId}/token-editions/{id}/estimate-gas/transfer',
