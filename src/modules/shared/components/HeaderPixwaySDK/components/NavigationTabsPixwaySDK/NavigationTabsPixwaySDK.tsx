961d25e6fc3521d29ec18b87dc429a9d323c0535
Marco Schmidt <marco.schmidt@w3block.io>
2022-10-25 17:10:34 -0300

    adjust on lateral menu

--- src/modules/shared/components/HeaderPixwaySDK/components/NavigationTabsPixwaySDK/NavigationTabsPixwaySDK.tsx
+++ src/modules/shared/components/HeaderPixwaySDK/components/NavigationTabsPixwaySDK/NavigationTabsPixwaySDK.tsx
@@ -1,6 +1,7 @@
 import { useMemo, useState } from 'react';
 
 import { PixwayAppRoutes } from '../../../../enums/PixwayAppRoutes';
+import { usePixwaySession } from '../../../../hooks/usePixwaySession';
 import useTranslation from '../../../../hooks/useTranslation';
 import { NavigationTabsPixwaySDKDesktop } from './components/NavigationTabsPixwaySDKDesktop';
 import { NavigationTabsPixwaySDKMobile } from './components/NavigationTabsPixwaySDKMobile';
@@ -34,6 +35,7 @@ export const NavigationTabsPixwaySDK = ({
 }: NavigationTabsPixwaySDKProps) => {
   const [translate] = useTranslation();
   const [openedTabs, setOpenedTabs] = useState<boolean>(false);
+  const { data: session } = usePixwaySession();
   const defaultTabs: NavigationTabsPixwaySDKTabs[] = useMemo(() => {
     if (!tabs) {
       return [
@@ -73,16 +75,18 @@ export const NavigationTabsPixwaySDK = ({
           classNames={classNames}
         />
       </div>
-      <div className="pw-block sm:pw-hidden">
-        <NavigationTabsPixwaySDKMobile
-          opened={opened ? opened : openedTabs}
-          toogleMenu={toggleTabsMemo}
-          signInRoute={signInRoute}
-          signUpRoute={signUpRoute}
-          tabs={defaultTabs}
-          classNames={classNames}
-        />
-      </div>
+      {session && tabs?.length === 0 ? null : (
+        <div className="pw-block sm:pw-hidden">
+          <NavigationTabsPixwaySDKMobile
+            opened={opened ? opened : openedTabs}
+            toogleMenu={toggleTabsMemo}
+            signInRoute={signInRoute}
+            signUpRoute={signUpRoute}
+            tabs={defaultTabs}
+            classNames={classNames}
+          />
+        </div>
+      )}
     </>
   );
 };
