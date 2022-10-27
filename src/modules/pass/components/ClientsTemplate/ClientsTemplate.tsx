ffe948d27f39307e5c095c9601667f7b7d1227b4
Danilo Schleu <daniloschleu@gmail.com>
2022-10-27 07:55:47 -0300

    adjusts layout

--- src/modules/pass/components/ClientsTemplate/ClientsTemplate.tsx
+++ src/modules/pass/components/ClientsTemplate/ClientsTemplate.tsx
@@ -89,50 +89,60 @@ export const ClientTemplate = () => {
 
   return isDevelopment ? (
     <BaseTemplate>
-      <div className="pw-flex pw-flex-col sm:pw-flex-row pw-p-[16px] pw-gap-[24px] sm:pw-gap-[16px] pw-items-center pw-border pw-border-[#E6E8EC] pw-rounded-[16px]">
-        <img
-          src={token.image}
-          alt={token.name}
-          className="pw-w-[319px] sm:pw-w-[216px] pw-h-[265px] sm:pw-h-[175px] pw-shadow-[2px_2px_10px_rgba(0,0,0,0.08)] pw-rounded-[20px]"
-        />
+      <div className="pw-flex pw-flex-col pw-p-[16px] pw-gap-[24px] sm:pw-gap-[16px] pw-border pw-border-[#E6E8EC] pw-rounded-[16px]">
+        <div className="pw-flex pw-flex-col sm:pw-flex-row pw-gap-[24px] sm:pw-gap-[16px]  pw-items-center">
+          <div className="pw-w-[319px] sm:pw-w-[216px] pw-h-[265px] sm:pw-h-[175px] pw-shadow-[2px_2px_10px_rgba(0,0,0,0.08)] pw-rounded-[20px] pw-overflow-hidden">
+            <img
+              src={token.image}
+              alt={token.name}
+              className="pw-w-full pw-h-full"
+            />
+          </div>
 
-        <div className="pw-flex pw-gap-4 pw-w-full">
-          <div className="pw-flex pw-flex-col sm:pw-w-[120px] pw-justify-center pw-items-center">
-            <div className="pw-text-[24px] pw-leading-[36px] pw-font-bold pw-text-[#295BA6] pw-text-center">
-              {shortDay[getDay(eventDate)]}
+          <div className="pw-flex pw-justify-start pw-gap-4 pw-w-full pw-flex-1">
+            <div className="pw-flex pw-flex-col sm:pw-w-[47px] pw-justify-center pw-items-center">
+              <div className="pw-text-[24px] pw-leading-[36px] pw-font-bold pw-text-[#295BA6] pw-text-center">
+                {shortDay[getDay(eventDate)]}
+              </div>
+              <div className="pw-text-[14px] pw-leading-[21px] pw-font-normal pw-text-[#777E8F] pw-text-center pw-w-[50px]">
+                {format(eventDate, 'dd MMM yyyy')}
+              </div>
+              <div className="pw-text-[15px] pw-leading-[23px] pw-font-semibold pw-text-[#353945] pw-text-center">
+                {format(eventDate, "HH'h'mm")}
+              </div>
             </div>
-            <div className="pw-text-[14px] pw-leading-[21px] pw-font-normal pw-text-[#777E8F] pw-text-center pw-w-[50px]">
-              {format(eventDate, 'dd MMM yyyy')}
-            </div>
-            <div className="pw-text-[15px] pw-leading-[23px] pw-font-semibold pw-text-[#353945] pw-text-center">
-              {format(eventDate, "HH'h'mm")}
+            <div className="pw-h-[152px] sm:pw-h-[101px] pw-bg-[#DCDCDC] pw-w-[1px]" />
+            <div className="pw-flex pw-flex-col pw-justify-center pw-flex-1">
+              <div className="pw-text-[18px] pw-leading-[23px] pw-font-bold pw-text-[#295BA6]">
+                {token.name}
+              </div>
+              <div className="pw-text-[14px] pw-leading-[21px] pw-font-normal pw-text-[#777E8F]">
+                {token.address.street}
+                {', '}
+                {token.address.city}
+                {' - '}
+                {token.address.country}
+              </div>
+              <div className="pw-text-[14px] pw-leading-[21px] pw-font-normal pw-text-[#777E8F]">
+                {token.address.cabin}
+              </div>
+              <div className="pw-text-[15px] pw-leading-[22px] pw-font-normal pw-text-[#353945] pw-mt-3">
+                <Trans
+                  i18nKey={'token>pass>totalValidedTokens'}
+                  tOptions={{ total: 100 }}
+                >
+                  Total tokens validados:{' '}
+                  <span className="pw-font-semibold">100</span>
+                </Trans>
+              </div>
             </div>
           </div>
-          <div className="pw-h-[152px] sm:pw-h-[101px] pw-bg-[#DCDCDC] pw-w-[1px]" />
-          <div className="pw-flex pw-flex-col pw-justify-center">
-            <div className="pw-text-[18px] pw-leading-[23px] pw-font-bold pw-text-[#295BA6]">
-              {token.name}
-            </div>
-            <div className="pw-text-[14px] pw-leading-[21px] pw-font-normal pw-text-[#777E8F]">
-              {token.address.street}
-              {', '}
-              {token.address.city}
-              {' - '}
-              {token.address.country}
-            </div>
-            <div className="pw-text-[14px] pw-leading-[21px] pw-font-normal pw-text-[#777E8F]">
-              {token.address.cabin}
-            </div>
-            <div className="pw-text-[15px] pw-leading-[22px] pw-font-normal pw-text-[#353945] pw-mt-3">
-              <Trans
-                i18nKey={'token>pass>totalValidedTokens'}
-                tOptions={{ total: 100 }}
-              >
-                Total tokens validados:{' '}
-                <span className="pw-font-semibold">100</span>
-              </Trans>
-            </div>
-          </div>
+        </div>
+
+        <div className="pw-w-full pw-flex pw-flex-1 pw-justify-end">
+          <button className="pw-bg-[#295BA6] hover:pw-bg-[#4194CD] pw-text-white pw-py-[7.5px] pw-px-6 pw-rounded-full pw-shadow-[0px_2px_4px_rgba(0,0,0,0.26)] pw-border-b pw-border-white">
+            Validar benefício
+          </button>
         </div>
       </div>
 
