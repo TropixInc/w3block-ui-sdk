f0a2960e7ad72c8b0b4320fad054d70db7652280
yanguyt <yanguyt@hotmail.com>
2022-11-08 10:09:54 -0300

    adjustments

--- src/modules/shared/hooks/useHasWallet/useHasWallet.tsx
+++ src/modules/shared/hooks/useHasWallet/useHasWallet.tsx
@@ -8,15 +8,20 @@ import useRouter from '../useRouter';
 export const useHasWallet = (
   redirectRoute: PixwayAppRoutes = PixwayAppRoutes.CONNECT_EXTERNAL_WALLET
 ) => {
-  const { data: profile, isLoading } = useProfile();
+  const { data: profile, isLoading, isSuccess } = useProfile();
 
   const router = useRouter();
 
   useEffect(() => {
-    if (!profile?.data.mainWallet && !isLoading && router.isReady) {
+    if (
+      !profile?.data.mainWallet &&
+      !isLoading &&
+      router.isReady &&
+      isSuccess
+    ) {
       router.push(redirectRoute);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [router.isReady, redirectRoute, isLoading]);
+  }, [router.isReady, redirectRoute, isLoading, isSuccess]);
   return;
 };
