b672772d25b508b5e26a45d52003bb1e74c7faa2
Danilo Schleu <daniloschleu@gmail.com>
2022-11-29 11:36:20 -0300

    Layout(np-7): adjusts layout (#80)

--- src/modules/tokens/components/PassTemplate/DetailPass.tsx
+++ src/modules/tokens/components/PassTemplate/DetailPass.tsx
@@ -14,7 +14,7 @@ export const DetailPass = ({
   const [isOpen, toggleOpen] = useToggle(true);
   return (
     <div className="pw-w-full pw-flex pw-flex-col pw-justify-center pw-items-center pw-py-[11px] pw-rounded-[16px] pw-border pw-border-[#EFEFEF] ">
-      <div className="pw-text-[#353945] pw-font-bold pw-text-[15px] pw-leading-[22.5px] pw-flex pw-gap-[10px] pw-p-[16px] pw-border-b pw-border-[#EFEFEF] pw-w-full pw-justify-center sm:pw-justify-start">
+      <div className="pw-text-[#353945] pw-font-bold pw-text-[18px] pw-leading-[22.5px] pw-flex pw-gap-[10px] pw-p-[16px] pw-border-b pw-border-[#EFEFEF] pw-w-full pw-justify-center sm:pw-justify-start">
         {title}
       </div>
       <div
