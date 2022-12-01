210f3582ea28fa0f2f734f562b33ca1578b80707
Marco Schmidt <marco.schmidt@w3block.io>
2022-12-01 16:02:05 -0300

    adjusts on verify sign and metamask button

--- src/modules/auth/components/ConnectWalletTemplate/GenerateTokenDialog.tsx
+++ src/modules/auth/components/ConnectWalletTemplate/GenerateTokenDialog.tsx
@@ -41,8 +41,9 @@ export const GenerateTokenDialog = ({
       classes={{
         dialogCard: '!pw-px-[98px] !pw-max-w-[653px]',
         actionContainer: '!pw-gap-x-15',
-        confirmButton: '!pw-py-3 !pw-w-full !pw-max-w-[200px]',
-        cancelButton: '!pw-py-3 !pw-w-full !pw-max-w-[200px]',
+        confirmButton:
+          '!pw-py-3 !pw-w-full !pw-bg-brand-primary !pw-max-w-[200px]',
+        cancelButton: '!pw-py-3 !pw-w-full !pw-bg-white !pw-max-w-[200px]',
       }}
     >
       <p className="pw-font-semibold pw-text-xl pw-leading-[23px] pw-text-black pw-mb-[53px] pw-text-center">
