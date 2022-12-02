10f2f756cb51ca2d3d1042e1716b622a15f89aff
Douglas S. Elias <douglassantoselias@gmail.com>
2022-12-02 10:32:23 -0300

    feat(pe-142): create theme context

---
+++ src/modules/storefront/contexts/ThemeContext/ThemeContext.tsx
@@ -0,0 +1,96 @@
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useEffect,
+  useState,
+} from 'react';
+import { useEffectOnce } from 'react-use';
+
+import { Template, TemplateData } from '../../interfaces';
+
+export const ThemeContext = createContext<IThemeContext | null>(null);
+interface IThemeContext {
+  defaultTheme: Template | null;
+  pageTheme: TemplateData | null;
+  setPageName: Dispatch<SetStateAction<string>>;
+}
+
+export const ThemeProvider = ({ children }: ThemeProviderProps) => {
+  const [defaultTheme, setDefaultTheme] = useState<Template | null>(null);
+  const [pageTheme, setPageTheme] = useState<TemplateData | null>(null);
+  const [pageName, setPageName] = useState('');
+
+  useEffectOnce(() => {
+    //requisição pra pegar valores default
+    // const baseURL = "https://api.w3block.io";
+    // const location = "primesea.io";
+    // const url = `${baseURL}/storefrontTheme?site=${location}`;
+    setDefaultTheme(sampleTemplate);
+  });
+
+  useEffect(() => {
+    // requisição pra pegar valores do user/página
+    // const baseURL = "https://api.w3block.io";
+    // const location = "primesea.io";
+    // const url = `${baseURL}/storeFrontPage?site=${location}&path=${pageName}`;
+
+    setPageTheme(sampleTemplateData);
+  }, [pageName]);
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        defaultTheme,
+        pageTheme,
+        setPageName,
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+const sampleTemplate: Template = {
+  header: {
+    bgColor: 'rgba(255,127,127,0.5)',
+    textColor: 'rgba(20,10,255,1)',
+  },
+  banner: {
+    bgColor: 'rgba(255,127,127,0.5)',
+    textColor: 'rgba(20,10,255,1)',
+  },
+};
+
+const sampleTemplateData: TemplateData = {
+  title: 'Home page',
+  items: [
+    {
+      type: 'header',
+      props: {
+        brandText: 'W3block Storefront Custom',
+        bgColor: 'rgba(255,127,127,0.5)',
+        textColor: 'rgba(20,10,255,1)',
+        links: [
+          {
+            label: 'Sobre',
+            type: 'internal',
+            value: 'about',
+            newWindow: false,
+          },
+          {
+            label: 'Nossos parceiros',
+            type: 'external',
+            value: 'https://www.nossosparceiros.com.br',
+            newWindow: true,
+          },
+        ],
+      },
+    },
+  ],
+};
