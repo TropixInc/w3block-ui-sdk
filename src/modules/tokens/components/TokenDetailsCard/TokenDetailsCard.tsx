79d44d04ccdd2d8fd71ea71366fd53b8fb62fa5d
yanguyt <yanguyt@hotmail.com>
2022-12-21 10:07:43 -0300

    fix - video tags without download and controls options

--- src/modules/tokens/components/TokenDetailsCard/TokenDetailsCard.tsx
+++ src/modules/tokens/components/TokenDetailsCard/TokenDetailsCard.tsx
@@ -173,6 +173,7 @@ export const TokenDetailsCard = ({
           )}
           <div className="pw-flex pw-justify-center">
             <ImageSDK
+              controls={true}
               className="pw-max-w-full pw-max-h-[351px] pw-object-contain pw-rounded-[12px] pw-overflow-hidden pw-shadow-[2px_2px_10px_rgba(0,0,0,0.08)]"
               src={mainImage}
               alt=""
