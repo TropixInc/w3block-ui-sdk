535577b5d20dd4774671d79e12344f822f7013bc
Douglas S. Elias <douglassantoselias@gmail.com>
2023-01-04 16:15:27 -0300

    fix: add translation for Cookies component

--- src/modules/storefront/components/Cookies.tsx
+++ src/modules/storefront/components/Cookies.tsx
@@ -1,17 +1,19 @@
 import { useLocalStorage } from 'react-use';
 
+import TranslatableComponent from '../../shared/components/TranslatableComponent';
+import useTranslation from '../../shared/hooks/useTranslation';
+
 export const Cookies = ({ data, defaultData }: CookiesProps) => {
+  const [translate] = useTranslation();
   const [value, setValue] = useLocalStorage('acceptedCookies', 'false');
 
-  const cookiesBgColor = data?.cookiesBgColor || defaultData.cookiesBgColor;
-  const cookiesTextColor =
-    data?.cookiesTextColor || defaultData.cookiesTextColor;
-  const cookiesButtonBgColor =
-    data?.cookiesButtonBgColor || defaultData.cookiesButtonBgColor;
-  const cookiesButtonTextColor =
-    data?.cookiesButtonTextColor || defaultData.cookiesButtonTextColor;
-  const privacyPolicyLinkColor =
-    data?.privacyPolicyLinkColor || defaultData.privacyPolicyLinkColor;
+  const {
+    cookiesBgColor,
+    cookiesTextColor,
+    cookiesButtonBgColor,
+    cookiesButtonTextColor,
+    privacyPolicyLinkColor,
+  } = { ...defaultData, ...data };
 
   const disclaimer = data?.disclaimer;
   const privacyPolicyLink = data?.privacyPolicyLink;
@@ -19,47 +21,44 @@ export const Cookies = ({ data, defaultData }: CookiesProps) => {
   if (value === 'true') return null;
 
   return (
-    <div
-      style={{ background: cookiesBgColor }}
-      className="pw-w-full sm:pw-h-[171px] pw-p-4 pw-flex pw-justify-center pw-font-poppins"
-    >
-      <div className="pw-flex pw-items-center pw-justify-between pw-h-full pw-container pw-flex-wrap lg:pw-flex-nowrap">
-        <p
-          className="pw-text-lg pw-max-w-[1300px]"
-          style={{ color: cookiesTextColor }}
-        >
-          {disclaimer}{' '}
-          <a
-            href={privacyPolicyLink}
-            target="_blank"
-            style={{ color: privacyPolicyLinkColor }}
-            rel="noreferrer"
+    <TranslatableComponent>
+      <div
+        style={{ background: cookiesBgColor }}
+        className="pw-w-full sm:pw-h-[171px] pw-p-4 pw-flex pw-justify-center pw-font-poppins"
+      >
+        <div className="pw-flex pw-items-center pw-justify-between pw-h-full pw-container pw-flex-wrap lg:pw-flex-nowrap">
+          <p
+            className="pw-text-lg pw-max-w-[1300px]"
+            style={{ color: cookiesTextColor }}
           >
-            Política de privacidade.
-          </a>
-        </p>
-        <button
-          style={{
-            background: cookiesButtonBgColor,
-            color: cookiesButtonTextColor,
-          }}
-          className="pw-border-none pw-h-[32px] pw-w-[109px] pw-rounded-md"
-          onClick={() => setValue('true')}
-        >
-          Estou ciente
-        </button>
+            {disclaimer}{' '}
+            <a
+              href={privacyPolicyLink}
+              target="_blank"
+              style={{ color: privacyPolicyLinkColor }}
+              rel="noreferrer"
+            >
+              {translate('storefront>cookies>privacyPolicy')}
+            </a>
+          </p>
+          <button
+            style={{
+              background: cookiesButtonBgColor,
+              color: cookiesButtonTextColor,
+            }}
+            className="pw-border-none pw-h-[32px] pw-w-[109px] pw-rounded-md"
+            onClick={() => setValue('true')}
+          >
+            {translate('storefront>cookies>iAgree')}
+          </button>
+        </div>
       </div>
-    </div>
+    </TranslatableComponent>
   );
 };
 
-export type CookiesData = {
-  cookiesBgColor?: string;
-  cookiesTextColor?: string;
-  cookiesButtonBgColor?: string;
-  cookiesButtonTextColor?: string;
+export type CookiesData = Partial<CookiesDefault> & {
   privacyPolicyLink?: string;
-  privacyPolicyLinkColor?: string;
   disclaimer?: string;
 };
 
