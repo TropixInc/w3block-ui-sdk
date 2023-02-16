636cb2bb7d7b3537c601a0ceb0e2149c67d5a4ca
yanguyt <yanguyt@hotmail.com>
2023-02-16 10:40:45 -0300

    fix - margin and padding adjustments on header

--- src/modules/shared/components/HeaderPixwaySDK/HeaderPixwaySDK.tsx
+++ src/modules/shared/components/HeaderPixwaySDK/HeaderPixwaySDK.tsx
@@ -104,11 +104,11 @@ const _HeaderPixwaySDK = ({
 
   return (
     <div
-      style={{ backgroundColor: bgColor, margin, padding }}
+      style={{ backgroundColor: bgColor, margin }}
       className="w-full pw-shadow-md"
     >
       <div
-        style={{ backgroundColor: bgColor }}
+        style={{ backgroundColor: bgColor, padding }}
         className={classNames(
           'pw-container pw-mx-auto pw-px-4 sm:pw-px-0',
           headerClassName ?? ''
