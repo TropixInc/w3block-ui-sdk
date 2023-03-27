0efe89955063c7881c665d13cf98af9bb7cefa94
yanguyt <yanguyt@hotmail.com>
2023-03-27 11:31:34 -0300

    adjust on function to remove slashes

--- src/modules/shared/utils/removeDuplicateSlahes.ts
+++ src/modules/shared/utils/removeDuplicateSlahes.ts
@@ -5,13 +5,16 @@ export const removeDoubleSlashesOnUrl = (url: string) => {
   const https = url.includes('https:');
   const http = url.includes('http:');
   let urlThreat = url
-    .replaceAll(https ? 'https://' : 'http://', '')
+    .replaceAll(https ? 'https://' : 'http://', https ? '${https}' : '${http}')
     .replace('///', '/')
     .replace('//', '/');
   if (!https && !http) {
     return urlThreat;
   } else {
-    urlThreat = (https ? 'https://' : 'http://') + urlThreat;
+    urlThreat = urlThreat.replaceAll(
+      https ? '${https}' : '${http}',
+      https ? 'https://' : 'http://'
+    );
     return urlThreat;
   }
 };
