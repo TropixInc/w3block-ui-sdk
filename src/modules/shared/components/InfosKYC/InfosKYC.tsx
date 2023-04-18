0f9110778ca51846cf4a8688ad59dc3716ecdb8b
fernando-pascoal <fernando.pascoal@tropix.io>
2023-04-18 12:57:07 -0300

    create section of kyc in my perfil

---
+++ src/modules/shared/components/InfosKYC/InfosKYC.tsx
@@ -0,0 +1,9 @@
+const InfosKYC = () => {
+  return (
+    <div className="pw-w-full pw-flex pw-flex-col pw-gap-[34px] pw-items-start pw-bg-white pw-rounded-[20px] pw-shadow-[2px_2px_10px] pw-shadow-[#00000014] pw-p-[34px]">
+      <p>teste</p>
+    </div>
+  );
+};
+
+export default InfosKYC;
