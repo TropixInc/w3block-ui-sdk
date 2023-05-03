d5b94a96dad980cc8e05d9ea072012832e76e982
yanguyt <yanguyt@hotmail.com>
2023-05-03 12:05:07 -0300

    adjustments

--- src/modules/checkout/components/CheckoutContainer/CheckoutContainer.tsx
+++ src/modules/checkout/components/CheckoutContainer/CheckoutContainer.tsx
@@ -30,7 +30,7 @@ export const CheckoutContainer = ({
             checkoutStatus={checkoutStatus}
           />
         </div>
-        <div className="lg:pw-h-[370px] lg:pw-w-[2px] pw-w-full pw-h-[2px] pw-bg-[#DCDCDC] pw-mt-[24px] lg:pw-mt-0" />
+        {/* <div className="lg:pw-h-[370px] lg:pw-w-[2px] pw-w-full pw-h-[2px] pw-bg-[#DCDCDC] pw-mt-[24px] lg:pw-mt-0" /> */}
         <div className="pw-flex pw-mt-[32px] lg:pw-mt-0">
           {/* <FAQ
             name={
