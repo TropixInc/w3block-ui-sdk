67c974744c6878abbe4c99f657b5cd318286f1bc
yanguyt <yanguyt@hotmail.com>
2023-05-09 15:30:02 -0300

    new id sdk

--- src/modules/auth/hooks/useSignUp/useSignUp.ts
+++ src/modules/auth/hooks/useSignUp/useSignUp.ts
@@ -8,6 +8,7 @@ import { PixwayAppRoutes } from '../../../shared/enums/PixwayAppRoutes';
 import { useCompanyConfig } from '../../../shared/hooks/useCompanyConfig';
 import { useGetW3blockIdSDK } from '../../../shared/hooks/useGetW3blockIdSDK';
 //import { useUtms } from '../../../shared/hooks/useUtms/useUtms';
+import { useUtms } from '../../../shared/hooks/useUtms/useUtms';
 import { removeDoubleSlashesOnUrl } from '../../../shared/utils/removeDuplicateSlahes';
 
 interface Payload {
@@ -23,15 +24,15 @@ interface Payload {
 
 export const useSignUp = () => {
   const getSDK = useGetW3blockIdSDK();
-  // const utms = useUtms();
+  const utms = useUtms();
   const { companyId, appBaseUrl, connectProxyPass } = useCompanyConfig();
   return useMutation([PixwayAPIRoutes.USERS], async (payload: Payload) => {
     const signUpPayload = payload;
-    // const ut = utms;
-    // if (utms.expires && utms?.expires > new Date().getTime()) {
-    //   delete ut.expires;
-    //   payload.utmParams = ut;
-    // }
+    const ut = utms;
+    if (utms.expires && utms?.expires > new Date().getTime()) {
+      delete ut.expires;
+      signUpPayload.utmParams = ut;
+    }
     const sdk = await getSDK();
     return sdk.api.auth.signUp({
       ...signUpPayload,
