ad92ef0540366bb7f1a024c05780c50723b0a497
fernando-pascoal <fernando.pascoal@tropix.io>
2023-05-15 12:59:53 -0300

    fix font size of inputs KYC in mobile

--- src/modules/shared/components/SmartInputs/InputPhone/index.tsx
+++ src/modules/shared/components/SmartInputs/InputPhone/index.tsx
@@ -57,7 +57,7 @@ const InputPhone = ({ label, name, docValue, docStatus }: InputPhoneProps) => {
           onChange={handleChange}
           placeholder="+XX XX XXXXX XXXX"
           className={classNames(
-            'pw-mt-1 pw-text-base pw-h-[46px] pw-text-[#969696] pw-leading-4 pw-w-full pw-shadow-[0_4px_15px_#00000012] !pw-rounded-lg pw-outline-none pw-bg-transparent pw-px-[10px] autofill:pw-bg-transparent'
+            'pw-mt-1 pw-text-base pw-h-[46px] pw-text-[#969696] pw-leading-4 pw-w-full pw-shadow-[0_4px_15px_#00000012] !pw-rounded-lg pw-outline-none pw-bg-transparent pw-px-[10px] autofill:pw-bg-transparent sm:pw-text-base'
           )}
         />
       </FormItemContainer>
--- src/modules/shared/components/SmartInputs/InputText/index.tsx
+++ src/modules/shared/components/SmartInputs/InputText/index.tsx
@@ -51,7 +51,7 @@ const InputText = ({ label, name, docValue, docStatus }: InputText) => {
           readOnly={docStatus && validateIfStatusKycIsReadonly(docStatus)}
           onChange={(e) => handleTextChange(e.target.value)}
           value={inputValue}
-          className="pw-mt-1 pw-text-base pw-h-[48px] pw-text-[#969696] pw-leading-4 pw-w-full !pw-rounded-lg pw-bg-transparent pw-px-[10px] autofill:pw-bg-transparent focus:pw-outline-none"
+          className="pw-mt-1 pw-text-base pw-h-[48px] pw-text-[#969696] pw-leading-4 pw-w-full !pw-rounded-lg pw-bg-transparent pw-px-[10px] autofill:pw-bg-transparent focus:pw-outline-none sm:pw-text-base"
         />
       </FormItemContainer>
       <p className="mt-5">
