2ed273bcb5f6f09cb21754d090234db75d1fabed
Marco Schmidt <marco.schmidt@w3block.io>
2023-06-23 17:43:28 -0300

    adjusts on user integration

--- src/modules/shared/components/RedirectTemplate/RedirectTemplate.tsx
+++ src/modules/shared/components/RedirectTemplate/RedirectTemplate.tsx
@@ -3,22 +3,31 @@ import { useEffect } from 'react';
 import useGetProductById from '../../../storefront/hooks/useGetProductById/useGetProductById';
 import { PixwayAppRoutes } from '../../enums/PixwayAppRoutes';
 import { useRouterConnect } from '../../hooks';
+import { useSessionUser } from '../../hooks/useSessionUser';
 import { Spinner } from '../Spinner';
 import TranslatableComponent from '../TranslatableComponent';
 
 const _RedirectTemplate = () => {
   const router = useRouterConnect();
+  const user = useSessionUser();
   const productId = (router.query.productId as string) ?? '';
   const { data: product } = useGetProductById(productId);
 
   useEffect(() => {
-    if (product?.slug) {
+    if (!user) {
+      router.pushConnect(PixwayAppRoutes.SIGN_IN, {
+        callbackPath: PixwayAppRoutes.PRODUCT_PAGE.replace(
+          '{slug}',
+          product?.slug ?? ''
+        ),
+      });
+    } else if (product?.slug && user) {
       router.pushConnect(
         PixwayAppRoutes.PRODUCT_PAGE.replace('{slug}', product?.slug ?? '')
       );
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [product?.slug]);
+  }, [product?.slug, user]);
 
   return (
     <div className="pw-h-[80vh]">
