1a0c64856a71d6ee6f3217b3501447092356ad3f
yanguyt <yanguyt@hotmail.com>
2023-08-03 11:54:37 -0300

    user report template

--- src/modules/business/templates/userReportTemplate/userReportTemplate.tsx
+++ src/modules/business/templates/userReportTemplate/userReportTemplate.tsx
@@ -1,8 +1,45 @@
 import { InternalPagesLayoutBase } from '../../../shared';
 import { ReactComponent as UserIcon } from '../../../shared/assets/icons/userOutlined.svg';
+import {
+  TableDefault,
+  TableHeaderItem,
+} from '../../../shared/components/TableDefault/TableDefault';
 import { ActionBusinessCardSDK } from '../../components/actionBusinessCardSDK';
 
 export const UserReportTemplate = () => {
+  const header: TableHeaderItem[] = [
+    {
+      key: 'status',
+      name: 'Status',
+      component: (item: any) => (
+        <div className="pw-flex pw-items-center pw-gap-1">
+          <div
+            style={{ backgroundColor: 'green' }}
+            className="pw-w-2 pw-h-2 pw-rounded-full"
+          ></div>
+          <p className="pw-text-slate-500 pw-text-sm pw-font-semibold">
+            {item.status}
+          </p>
+        </div>
+      ),
+    },
+    {
+      key: 'client',
+      name: 'Cliente',
+    },
+    {
+      key: 'cpf',
+      name: 'CPF',
+    },
+    {
+      key: 'quantity',
+      name: 'Qtde.',
+    },
+    {
+      key: 'date',
+      name: 'Data',
+    },
+  ];
   return (
     <InternalPagesLayoutBase>
       <div className=" pw-p-6 pw-bg-white pw-rounded-[20px] pw-shadow pw-flex-col pw-justify-start pw-items-start">
@@ -16,6 +53,15 @@ export const UserReportTemplate = () => {
           buttonText="Pagar"
           onClick={() => console.log('teste')}
         />
+        <div className="pw-flex pw-justify-between pw-items-center pw-mt-8">
+          <p className="pw-text-zinc-800 pw-text-lg pw-font-medium pw-leading-[23px]">
+            Relatório de uso
+          </p>
+          <button className="pw-text-zinc-700 pw-text-[15px] pw-font-semibold ">
+            Ver relatório completo
+          </button>
+        </div>
+        <TableDefault className="pw-mt-[20px]" data={[{}]} header={header} />
       </div>
     </InternalPagesLayoutBase>
   );
