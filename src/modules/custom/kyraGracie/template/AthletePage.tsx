4fd5cc8e44a3d9cc5cee8b1c6491c6605bf4f0a0
Marco Schmidt <antonio18900@gmail.com>
2024-01-16 16:42:51 -0300

    adjusts on student historic for wjjc

--- src/modules/custom/kyraGracie/template/AthletePage.tsx
+++ src/modules/custom/kyraGracie/template/AthletePage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 const Shimmer = lazy(() =>
   import('../../../shared/components/Shimmer').then((mod) => ({
@@ -7,11 +8,17 @@ const Shimmer = lazy(() =>
 
 import { lazy } from 'react';
 
+import { Disclosure } from '@headlessui/react';
+import classNames from 'classnames';
 import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
+import ChevronDown from '../../../../modules/shared/assets/icons/arrowDown.svg?react';
 import { Spinner } from '../../../shared/components/Spinner';
 import { useIsProduction } from '../../../shared/hooks/useIsProduction';
 import { useDynamicApi } from '../../../storefront/provider/DynamicApiProvider';
+import { createDateRange } from '../../utils/createDateRange';
+import { groupByDateRange } from '../../utils/groupByDateRange';
 import blackBelt from '../assets/black_belt.png';
 import blueBelt from '../assets/blue_belt.png';
 import brownBelt from '../assets/brown_belt.png';
@@ -25,17 +32,23 @@ import {
   BeltColor,
   useGetAthlete,
 } from '../hooks/useGetAthlete';
-
 export const AthletePage = () => {
   const { datasource, loading } = useDynamicApi();
   const { data, isLoading } = useGetAthlete(
     datasource?.athlete?.data[0]?.id ?? ''
   );
-
   const isProduction = useIsProduction();
+  const beltsData = datasource?.athlete?.data[0]?.attributes.titles.filter(
+    (e: any) => e.type === 'belt'
+  );
+  const titles = datasource?.athlete?.data[0]?.attributes.titles;
+  const dates = beltsData?.map((e: any) => {
+    return e?.date;
+  });
+  const datesRange = createDateRange(dates ?? []);
+  const groupTitles = groupByDateRange(titles ?? [], datesRange ?? []);
+
   const belts = [
-    BeltColor.YELLOW,
-    BeltColor.ORANGE,
     BeltColor.BLUE,
     BeltColor.PURPLE,
     BeltColor.BROWN,
@@ -54,6 +67,15 @@ export const AthletePage = () => {
     Vermelha: 'Red',
     Coral: 'Coral',
   };
+
+  const gradeMap = {
+    degree_1: '1º Degree',
+    degree_2: '2º Degree',
+    degree_3: '3º Degree',
+    degree_4: '4º Degree',
+  };
+
+  type Grade = 'degree_1' | 'degree_2' | 'degree_3' | 'degree_4';
 
   type Belt =
     | 'Amarela'
@@ -86,27 +108,6 @@ export const AthletePage = () => {
     }
   };
 
-  const getZIndexNumberByIndex = (index: number) => {
-    switch (index) {
-      case 0:
-        return 8;
-      case 1:
-        return 7;
-      case 2:
-        return 6;
-      case 3:
-        return 5;
-      case 4:
-        return 4;
-      case 5:
-        return 3;
-      case 6:
-        return 2;
-      case 7:
-        return 1;
-    }
-  };
-
   const getPlaceholder = (): AthleteInterface => {
     return {
       athleteIdentification: 0,
@@ -128,10 +129,10 @@ export const AthletePage = () => {
           Date.parse(
             datasource?.athlete?.data[0]?.attributes?.birthdate + 'T12:00:00'
           ),
-          'PP'
+          'P',
+          { locale: ptBR }
         );
     } catch (e) {
-      // eslint-disable-next-line no-console
       console.log(e);
     }
     return '';
@@ -147,8 +148,7 @@ export const AthletePage = () => {
     return datasource?.athlete?.data?.length ? (
       <div
         style={{
-          backgroundImage: `url('https://res.cloudinary.com/tropix/image/upload/v1686157813/assets/certificate/kyra/bg-papiro2_niym17.png')`,
-          backgroundSize: '70px',
+          backgroundImage: `url('https://res.cloudinary.com/tropix/image/upload/v1705433628/wwjjc/assets/background-historic.png')`,
           backgroundRepeat: 'repeat',
         }}
         className="pw-bg-[#fffefb]"
@@ -197,18 +197,18 @@ export const AthletePage = () => {
               </p>
             )}
             {!datasource ? (
-              <Shimmer className="pw-min-h-[17px] pw-min-w-[150px] pw-mt-2" />
-            ) : (
-              <p className="pw-text-center pw-text-black pw-font-[500] pw-mt-2 pw-text-[16px]">
-                {birthDate() ?? getPlaceholder().athleteBirthdate}
-              </p>
-            )}
-            {!datasource ? (
               <Shimmer className="pw-min-h-[17px] pw-min-w-[130px] pw-mt-2" />
             ) : (
               <p className="pw-text-center pw-text-black pw-font-[500] pw-mt-2 pw-text-[16px]">
                 {datasource?.athlete?.data[0]?.attributes?.gender ??
                   getPlaceholder().athleteGender}
+              </p>
+            )}
+            {!datasource ? (
+              <Shimmer className="pw-min-h-[17px] pw-min-w-[150px] pw-mt-2" />
+            ) : (
+              <p className="pw-text-center pw-text-black pw-font-[500] pw-mt-2 pw-text-[16px]">
+                {birthDate() !== '' ? `(${birthDate()})` : ''}
               </p>
             )}
             {!datasource ? (
@@ -219,18 +219,28 @@ export const AthletePage = () => {
                   getPlaceholder().athleteNationality}
               </p>
             )}
-            <div className="pw-mt-[60px]">
+            <div className="pw-mt-[60px] pw-max-w-[580px] pw-w-full pw-mx-auto pw-flex pw-flex-col pw-gap-2">
               {belts.map((belt, index) => {
                 const titlesFiltered =
                   datasource?.athlete?.data[0]?.attributes?.titles?.filter(
                     (item: any) => item?.type === 'belt'
                   );
+
                 const respectiveBelt = titlesFiltered?.find(
                   (item: any) => beltMap[item?.belt as Belt] == belt
                 );
+
+                const rangeTitles = groupTitles.find((res: any[]) =>
+                  res.some(
+                    (res) =>
+                      res?.type === 'belt' && beltMap[res?.belt as Belt] == belt
+                  )
+                );
+
                 const respectiveToken = data?.items?.find(
                   (item: any) => item?.tokenData?.beltColor === belt
                 );
+
                 const date = () => {
                   try {
                     if (
@@ -241,112 +251,132 @@ export const AthletePage = () => {
                     else if (respectiveBelt?.date)
                       return format(
                         Date.parse(respectiveBelt?.date + 'T12:00:00'),
-                        'PP'
+                        'P',
+                        { locale: ptBR }
                       );
                   } catch (e) {
-                    // eslint-disable-next-line no-console
                     console.log(e);
                   }
                   return '';
                 };
+
                 return (
-                  <div
-                    key={belt}
-                    style={{ zIndex: getZIndexNumberByIndex(index) }}
-                    className="pw-flex pw-items-start pw-gap-8 pw-relative"
-                  >
-                    <div className="pw-flex-1 pw-justify-end">
-                      {respectiveToken != null ? (
-                        <a
-                          target={`${respectiveToken ? '_blank' : ''}`}
-                          href={
-                            respectiveToken
-                              ? `https://pdf${
-                                  !isProduction ? '.stg' : ''
-                                }.w3block.io/certification/${
-                                  respectiveToken?.contractAddress
-                                }/${respectiveToken?.chainId}/${
-                                  respectiveToken?.tokenId
-                                }?preview`
-                              : undefined
-                          }
-                          onClick={() => false}
-                          rel="noreferrer"
+                  <Disclosure key={index}>
+                    {({ open }) => (
+                      <>
+                        <Disclosure.Button
+                          disabled={respectiveBelt === undefined}
+                          className={`${
+                            respectiveBelt === undefined ? 'pw-opacity-60' : ''
+                          } pw-p-[9px_12px] !pw-bg-[#F7F7F7] pw-text-black pw-font-bold pw-text-base flex pw-w-full pw-justify-between pw-items-center`}
                         >
-                          <img
-                            className=" pw-w-[150px] sm:pw-w-[200px] pw-object-contain"
-                            src={getBeltImage(belt)}
-                            alt={respectiveToken?.name ?? belt}
+                          <div className="pw-flex pw-gap-2">
+                            <img
+                              className=" pw-w-[30px] sm:pw-w-[45px] pw-object-contain"
+                              src={getBeltImage(belt)}
+                              alt={''}
+                            />
+                            {date() !== '' ? `(${date()})` : ''}
+                            <p>{belt + 'Belt'}</p>
+                          </div>
+                          <ChevronDown
+                            className={classNames(
+                              'pw-stroke-[#000000]',
+                              open ? 'pw-rotate-180' : ''
+                            )}
                           />
-                        </a>
-                      ) : (
-                        <img
-                          className=" pw-w-[150px] sm:pw-w-[200px] pw-object-contain"
-                          src={getBeltImage(belt)}
-                          alt={''}
-                        />
-                      )}
-                    </div>
+                        </Disclosure.Button>
+                        <Disclosure.Panel className="pw-p-[12px] pw-bg-[#F7F7F7] pw-flex pw-flex-col pw-gap-5">
+                          {rangeTitles?.map((res: any, index: any) => {
+                            const date = () => {
+                              if (res?.date && res?.date?.includes('-01-01'))
+                                return res?.date?.slice(0, 4);
+                              else if (res?.date)
+                                return format(
+                                  Date.parse(res?.date + 'T12:00:00'),
+                                  'P',
+                                  { locale: ptBR }
+                                );
+                              else return '';
+                            };
 
-                    <div className="pw-flex pw-flex-col pw-items-center pw-justify-center">
-                      <div className="pw-w-[27px] pw-h-[27px] pw-rounded-full pw-border pw-border-slate-300 pw-bg-white pw-flex pw-items-center pw-justify-center">
-                        {respectiveBelt != null && (
-                          <div className="pw-w-[17px] pw-h-[17px] pw-rounded-full pw-bg-[#01C458]"></div>
-                        )}
-                      </div>
-                      {index < belts.length - 1 && (
-                        <div className="pw-h-[200px] pw-w-[8px] pw-flex pw-items-center pw-justify-center pw-border pw-bg-white">
-                          {!respectiveBelt?.currentBelt &&
-                          respectiveBelt != null ? (
-                            <div className="pw-h-[230px] pw-w-[6px] pw-rounded-full pw-bg-[#01C458]"></div>
-                          ) : null}
-                        </div>
-                      )}
-                    </div>
+                            const title = () => {
+                              if (res?.type === 'belt') return belt + 'Belt';
+                              else if (res?.type === 'championship')
+                                return res?.tournamentName ?? '';
+                              else if (res?.type === 'grade')
+                                return gradeMap[res?.degree as Grade] ?? '';
+                              else return '';
+                            };
 
-                    <div className="pw-flex-1">
-                      {respectiveBelt != null && (
-                        <a
-                          target={`${respectiveToken ? '_blank' : ''}`}
-                          href={
-                            respectiveToken
-                              ? `https://pdf${
-                                  !isProduction ? '.stg' : ''
-                                }.w3block.io/certification/${
-                                  respectiveToken?.contractAddress
-                                }/${respectiveToken?.chainId}/${
-                                  respectiveToken?.tokenId
-                                }?preview`
-                              : undefined
-                          }
-                          rel="noreferrer"
-                        >
-                          <p className="pw-text-[16px] pw-font-[700] pw-text-black pw-flex">
-                            {respectiveBelt?.instructor?.data?.attributes
-                              ?.name ??
-                              respectiveToken?.tokenData?.graduationTeacher ??
-                              ''}
-                            {respectiveToken ? (
-                              <img
-                                src="https://res.cloudinary.com/tropix/image/upload/v1702925417/wwjjc/assets/badge_jqif6u.svg"
-                                width={15}
-                                height={15}
-                                className="pw-ml-[7px]"
-                              ></img>
-                            ) : null}
-                          </p>
-                          <p className="pw-text-[14px] pw-font-[600] pw-text-black ">
-                            {respectiveBelt?.academy?.data?.attributes?.name ??
-                              respectiveToken?.tokenData?.graduationAcademy ??
-                              ''}
-                          </p>
-                          <p className="pw-text-[12px] pw-font-[500] pw-text-black ">
-                            {date() ?? ''}
-                          </p>
-                        </a>
-                      )}
-                    </div>
-                  </div>
+                            const subtitle = () => {
+                              const placement = (placement: number) => {
+                                if (placement === 1) return 'st';
+                                else if (placement === 2) return 'nd';
+                                else if (placement === 3) return 'rd';
+                                else return 'th';
+                              };
+                              if (
+                                res?.type === 'belt' ||
+                                res?.type === 'grade'
+                              ) {
+                                if (
+                                  res?.instructor?.data?.attributes?.name &&
+                                  res?.academy?.data?.attributes?.name
+                                )
+                                  return (
+                                    res?.instructor?.data?.attributes?.name +
+                                      ', ' +
+                                      res?.academy?.data?.attributes?.name ?? ''
+                                  );
+                                else return '';
+                              } else if (res?.type === 'championship')
+                                return (
+                                  res?.tournamentPlacement +
+                                  placement(res?.tournamentPlacement) +
+                                  ' place'
+                                );
+                              else return '';
+                            };
+
+                            return (
+                              <a
+                                target={`${
+                                  respectiveToken && res.type === 'belt'
+                                    ? '_blank'
+                                    : ''
+                                }`}
+                                href={
+                                  respectiveToken && res.type === 'belt'
+                                    ? `https://pdf${
+                                        !isProduction ? '.stg' : ''
+                                      }.w3block.io/certification/${
+                                        respectiveToken?.contractAddress
+                                      }/${respectiveToken?.chainId}/${
+                                        respectiveToken?.tokenId
+                                      }?preview`
+                                    : undefined
+                                }
+                                key={index}
+                                className={`${
+                                  respectiveToken && res.type === 'belt'
+                                    ? ''
+                                    : 'pw-opacity-60'
+                                } pw-text-black pw-font-bold pw-text-base`}
+                              >
+                                {`${
+                                  date() !== '' ? `(${date()})` : ''
+                                } ${title()}`}
+                                <span className="pw-font-light pw-text-xs">
+                                  {' ' + subtitle()}
+                                </span>
+                              </a>
+                            );
+                          })}
+                        </Disclosure.Panel>
+                      </>
+                    )}
+                  </Disclosure>
                 );
               })}
             </div>
