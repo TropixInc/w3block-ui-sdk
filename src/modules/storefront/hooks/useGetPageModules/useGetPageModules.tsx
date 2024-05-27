08d66c0157e694c3c30ac8a563b240f81c8ba4a7
Fernando <fernandodevpascoal@gmail.com>
2024-05-27 14:16:51 -0300

    ajuste

--- src/modules/storefront/hooks/useGetPageModules/useGetPageModules.tsx
+++ src/modules/storefront/hooks/useGetPageModules/useGetPageModules.tsx
@@ -14,9 +14,9 @@ export const useGetPageModules = (disabled = false) => {
   useEffect(() => {
     if (window) {
       if (!window.location.href.includes('/product/slug')) {
-        setHref('https://foodbusters.stg.w3block.io/' + '?' + Date.now());
+        //setHref('https://foodbusters.stg.w3block.io/' + '?' + Date.now());
         //setHref('https://hashdex.stg.w3block.io/'+ '?' + Date.now());
-        //setHref(window.location.href);
+        setHref(window.location.href);
       }
     }
   }, []);
