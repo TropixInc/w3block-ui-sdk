6d828d73c3b0721b0549738625674caa7f73b240
Marco Schmidt <antonio18900@gmail.com>
2024-12-27 13:55:00 -0300

    exported card component

--- src/modules/checkout/components/index.ts
+++ src/modules/checkout/components/index.ts
@@ -2,3 +2,4 @@ export * from './CheckoutHeader';
 export * from './CheckoutContainer';
 export * from './CheckoutInfo';
 export * from './CheckoutPayment';
+export * from './CheckoutPaymentComponent';
