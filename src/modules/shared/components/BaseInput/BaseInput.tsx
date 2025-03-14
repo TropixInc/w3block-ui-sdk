081a9621ff75aa678acb9fef94bc07c955f5aa7d
Fernando <fernandodevpascoal@gmail.com>
2025-03-14 08:45:48 -0400

    change deps

--- src/modules/shared/components/BaseInput/BaseInput.tsx
+++ src/modules/shared/components/BaseInput/BaseInput.tsx
@@ -5,7 +5,7 @@ import {
   SetStateAction,
   useState,
 } from 'react';
-import ReactInputMask from 'react-input-mask';
+/* import ReactInputMask from 'react-input-mask'; */
 
 import classNames from 'classnames';
 
@@ -174,12 +174,13 @@ export const BaseInput = ({
           <SearchIcon className="pw-stroke-black pw-w-5 pw-pb-[2px]" />
         ) : null}
         {mask ? (
-          <ReactInputMask
+          <></>
+        ) : /*   <ReactInputMask
             className={`pw-w-full pw-h-full focus:pw-outline-none pw-flex`}
             mask={mask as string}
             {...props}
-          />
-        ) : textarea ? (
+          /> */
+        textarea ? (
           <textarea
             name={props.name}
             id={props.id}
