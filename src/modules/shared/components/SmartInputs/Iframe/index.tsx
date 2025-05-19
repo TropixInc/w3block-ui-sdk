76c3b0a8c1adce1ea5883cc626e23fa7af7ad878
Marco Schmidt <antonio18900@gmail.com>
2025-05-19 12:20:54 -0300

    adjusts on iframe

--- src/modules/shared/components/SmartInputs/Iframe/index.tsx
+++ src/modules/shared/components/SmartInputs/Iframe/index.tsx
@@ -6,15 +6,21 @@ interface Params {
 
 export const IframeInput = ({ docValue }: Params) => {
   const iframeRef = useRef(null);
+  const iframe = document?.getElementById('iframe-kyc') as HTMLIFrameElement;
+  const iframeURL = iframe?.src;
   if (docValue) {
     return (
-      <iframe
-        ref={iframeRef}
-        src={docValue}
-        className="pw-w-full pw-h-full"
-        style={{ minHeight: '500px' }}
-        allow="autoplay; camera; microphone"
-      />
+      <>
+        <iframe
+          ref={iframeRef}
+          src={docValue}
+          id={'iframe-kyc'}
+          className="pw-w-full pw-h-full"
+          style={{ minHeight: '500px' }}
+          allow="autoplay; camera; microphone; gyroscope; magnetometer; geolocation; accelerometer; ambient-light-sensor"
+        />
+        <div className="pw-mt-3">{iframeURL}</div>
+      </>
     );
   }
   return null;
