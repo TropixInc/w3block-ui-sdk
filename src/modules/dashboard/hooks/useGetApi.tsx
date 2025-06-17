1b367a2c42061c22cea0cba67c9285d7146a317d
Marco Schmidt <antonio18900@gmail.com>
2025-06-17 09:01:07 -0300

    adjusted api

--- src/modules/dashboard/hooks/useGetApi.tsx
+++ src/modules/dashboard/hooks/useGetApi.tsx
@@ -13,8 +13,8 @@ export const useGetApi = ({ address, enabled }: Params) => {
     () => {
       return axios.get(
         address
-          ? `https://cms.zuca.ai/api/restaurantes?populate[image][*]&filters[active]=true&sort[1]=name&sort[0]=weight:desc&pagination[pageSize]=100&filters[walletAddress]=${address}`
-          : 'https://cms.zuca.ai/api/restaurantes?populate[image][*]&filters[active]=true&sort[1]=name&sort[0]=weight:desc&pagination[pageSize]=100'
+          ? `https://cms.zuca.ai/api/restaurantes?populate[image][*]&filters[active]=true&sort[1]=name&sort[0]=name:asc&pagination[pageSize]=100&filters[walletAddress]=${address}`
+          : 'https://cms.zuca.ai/api/restaurantes?populate[image][*]&filters[active]=true&sort[1]=name&sort[0]=name:asc&pagination[pageSize]=100'
       );
     },
     { enabled: enabled }
